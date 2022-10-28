588d36ff738777fc60d6bc59507b4c0ae39de24e
Alisa Semenova <salisa4227@gmail.com>
2022-10-28 12:42:30 +0300

    Саруман и Ланнистеры.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int lannistersInfairymanCount = 1236;//переменная может измениться,например кто - то погибнет
+        int sarumanOrcsCount = 598;//переменная может измениться,например кто - то погибнет
+        int sarumanAndLannistersArmyCount = lannistersInfairymanCount + sarumanOrcsCount;
+        System.out.println("переменная lannistersInfairymanCount: " + lannistersInfairymanCount);
+        System.out.println("переменная sarumanOrcsCount: " + sarumanOrcsCount);
+        System.out.println("переменная sarumanAndLannistersArmyCount: " + sarumanAndLannistersArmyCount);
+    }
+}
